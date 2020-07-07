fe1632a3dd1385cedfc74b5a51f3a3428345d56f
Shabuktagin Photon Khan <khan.photon@gmail.com>
2020-07-07 19:53:52 +0600

    fix: remove dat gui from the project

--- src/Components/Anime/ThreeJS/Box/index.tsx
+++ src/Components/Anime/ThreeJS/Box/index.tsx
@@ -2,12 +2,10 @@ import * as React from 'react'
 import { useFrame } from 'react-three-fiber'
 
 import { Glitch } from '../Glitch'
-// @ts-ignore
-import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
 
 export const Box = ({ colors, history, wireframe = false, redirectURL, ...props }: any) => {
     const mesh: any = React.useRef()
-    let rotation = React.useRef({ x: 30, y: 30, z: 0 }).current
+    const [ rotation, setRotation] = React.useState({ x: 30, y: 30, z: 0 })
     const [hovered, setHover] = React.useState(false)
     const [active, setActive] = React.useState(false)
 
@@ -20,15 +18,8 @@ export const Box = ({ colors, history, wireframe = false, redirectURL, ...props 
         }
         mesh.current.rotation.x += rotation.x * 0.001
         mesh.current.rotation.y += rotation.y * 0.001
+        mesh.current.rotation.z += rotation.z * 0.001
     })
-
-    React.useEffect(() => {
-        let gui = new GUI()
-        gui.add(rotation, 'x', 0, 100).name('Rotation X')
-        gui.add(rotation, 'y', 0, 100).name('Rotation Y')
-        gui.add(rotation, 'z', 0, 100).name('Rotation Z')
-        gui.close()
-    }, [])
 
     return (
         <mesh
@@ -41,30 +32,32 @@ export const Box = ({ colors, history, wireframe = false, redirectURL, ...props 
             onPointerOver={(event) => setHover(true)}
             onPointerOut={(event) => setHover(false)}
             {...props}>
-            {
-                hovered ? (
-                    <>
-                        <sphereGeometry
-                            attach={`geometry`}
-                            args={[5, 32, 32]} />
-                        <meshBasicMaterial
-                            attach={'material'}
-                            color={colors.primaryColor}
-                            wireframe={wireframe} />
-                        <Glitch />
-                    </>
-                ) : (
+            <>
+                {
+                    hovered ? (
                         <>
-                            <boxGeometry
+                            <sphereGeometry
                                 attach={`geometry`}
-                                args={[5, 5, 5]} />
-                            <meshLambertMaterial
-                                wireframe={wireframe}
+                                args={[5, 32, 32]} />
+                            <meshBasicMaterial
                                 attach={'material'}
-                                color={colors.primaryColor} />
+                                color={colors.primaryColor}
+                                wireframe={wireframe} />
+                            <Glitch />
                         </>
-                    )
-            }
+                    ) : (
+                            <>
+                                <boxGeometry
+                                    attach={`geometry`}
+                                    args={[5, 5, 5]} />
+                                <meshLambertMaterial
+                                    wireframe={wireframe}
+                                    attach={'material'}
+                                    color={colors.primaryColor} />
+                            </>
+                        )
+                }
+            </>
         </mesh>
     )
 }
