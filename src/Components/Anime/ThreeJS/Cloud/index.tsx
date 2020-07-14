43d69b06a14aa1245e62753ff22323ccd1edc46f
Shabuktagin Photon Khan <khan.photon@gmail.com>
2020-07-14 19:32:04 +0600

    fix: improved the logic for the cloud

--- src/Components/Anime/ThreeJS/Cloud/index.tsx
+++ src/Components/Anime/ThreeJS/Cloud/index.tsx
@@ -10,10 +10,6 @@ export const Cloud = ({ noOfClouds = 100, colors, frequency }: any) => {
     const [texture, setTexture] = React.useState()
     const [clouds, setClouds]: any = React.useState()
 
-    useFrame(() => {
-
-    })
-
     React.useEffect(() => {
         const onSuccessLoad = (mapTexture: any) => setTexture(mapTexture)
         loader.load('/Cloud/cloud.png', onSuccessLoad)
@@ -21,43 +17,32 @@ export const Cloud = ({ noOfClouds = 100, colors, frequency }: any) => {
     }, [])
 
     React.useEffect(() => {
-        const preparedTexture = (
-            <>
-                {
-                    texture ? (
-                        <>
-                            {
-                                [...new Array(noOfClouds)].map((value, key) => {
-                                    return (
-                                        <mesh
-                                            key={key}
-                                            rotation={[
-                                                0,
-                                                - 3 * Math.PI / 4,
-                                                0
-                                            ]}
-                                            position={[
-                                                RandomRange({ min: - window.innerWidth, max: 0 }),
-                                                RandomRange({ min: 120, max: 200 }),
-                                                RandomRange({ min: -800, max: -1200 }),
-                                            ]}>
-                                            <planeGeometry
-                                                attach={`geometry`}
-                                                args={[100, 100, 100]} />
-                                            <meshStandardMaterial
-                                                side={THREE.DoubleSide}
-                                                attach={`material`}
-                                                map={texture}
-                                                transparent={true} />
-                                        </mesh>
-                                    )
-                                })
-                            }
-                        </>
-                    ) : <></>
-                }
-            </>
-        )
+        const preparedTexture = texture ?
+            [...new Array(noOfClouds)].map((value, key) => {
+                return (
+                    <mesh
+                        key={key}
+                        rotation={[
+                            0,
+                            - 3 * Math.PI / 4,
+                            0
+                        ]}
+                        position={[
+                            RandomRange({ min: - window.innerWidth, max: 0 }),
+                            RandomRange({ min: 120, max: 200 }),
+                            RandomRange({ min: -800, max: -1200 }),
+                        ]}>
+                        <planeGeometry
+                            attach={`geometry`}
+                            args={[100, 100, 100]} />
+                        <meshStandardMaterial
+                            side={THREE.DoubleSide}
+                            attach={`material`}
+                            map={texture}
+                            transparent={true} />
+                    </mesh>
+                )
+            }) : <></>
         setClouds(preparedTexture)
     }, [texture])
 
@@ -65,7 +50,7 @@ export const Cloud = ({ noOfClouds = 100, colors, frequency }: any) => {
     return (
         <>
             <pointLight
-                power={frequency !== 0 && Math.random() > 0.95 ? frequency * 0.05 : 1 }
+                power={frequency !== 0 && Math.random() > 0.95 ? frequency * 0.07 : 1 }
                 position={[-100, 0, -1000]}
                 args={[colors.primaryColor, 1, 5000, 1.7]} />
             { clouds }
