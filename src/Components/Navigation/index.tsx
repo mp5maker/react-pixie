d26591657077a7aace78076768423e8f2d993c2f
Shabuktagin Photon Khan <khan.photon@gmail.com>
2020-07-20 17:29:33 +0600

    feat: add dog svg in about me

--- src/Components/Navigation/index.tsx
+++ src/Components/Navigation/index.tsx
@@ -5,6 +5,7 @@ import { NavLink } from 'react-router-dom'
 import * as Routes from '../../Constants/Routes'
 import { Colors } from '../../Constants/Colors'
 import { AppContext } from '../../AppContext'
+import { Dog } from '../Dog'
 
 import "./styles.scss"
 
@@ -15,23 +16,28 @@ export const Navigation = ({ history }: any) => {
     const list = [
         {
             label: t(`ABOUT_ME`),
-            route: Routes.ROOT
+            route: Routes.ROOT,
+            key: `about-me`
         },
         {
             label: t(`EXPERIENCE`),
-            route: Routes.EXPERIENCE
+            route: Routes.EXPERIENCE,
+            key: `experience`
         },
         // {
         //     label: t(`SKYBOX`),
-        //     route: Routes.SKYBOX
+        //     route: Routes.SKYBOX,
+        //      key: `skybox`
         // },
         {
             label: t(`RAIN`),
-            route: Routes.RAIN
+            route: Routes.RAIN,
+            key: `rain`
         },
         {
             label: t(`BIRD`),
-            route: Routes.BIRD
+            route: Routes.BIRD,
+            key: `bird`
         },
     ]
 
@@ -58,7 +64,13 @@ export const Navigation = ({ history }: any) => {
                                                 // @ts-ignore
                                                 color: Colors[theme].primaryColor
                                             }}>
-                                            { item.label }
+                                            <span className={`mr-2`}>{ item.label }</span>
+                                            <Dog
+                                                key={item.key}
+                                                width={40}
+                                                height={37.3}
+                                                colors={Colors}
+                                                theme={theme} />
                                         </NavLink>
                                     </React.Fragment>
 
