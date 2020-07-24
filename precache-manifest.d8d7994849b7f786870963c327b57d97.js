1007f2da826e8d6209d7f9e8f35dd404fd3d5f14
Shabuktagin Photon Khan <khan.photon@gmail.com>
2020-07-24 18:01:25 +0600

    Updates

---
+++ precache-manifest.d8d7994849b7f786870963c327b57d97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8d082d581cca386bd54d8bab15c9bd4",
+    "url": "/react-pixie/index.html"
+  },
+  {
+    "revision": "8e61677b4715c21ef3ee",
+    "url": "/react-pixie/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5087568b484fd4137180",
+    "url": "/react-pixie/static/css/main.a8726362.chunk.css"
+  },
+  {
+    "revision": "8e61677b4715c21ef3ee",
+    "url": "/react-pixie/static/js/2.eb920489.chunk.js"
+  },
+  {
+    "revision": "5c48ad5b2cbd7e3756d023aae9e97f14",
+    "url": "/react-pixie/static/js/2.eb920489.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5087568b484fd4137180",
+    "url": "/react-pixie/static/js/main.dc10430f.chunk.js"
+  },
+  {
+    "revision": "a45173a58ab8fd9e9660",
+    "url": "/react-pixie/static/js/runtime-main.0fa4f835.js"
+  }
+]);
