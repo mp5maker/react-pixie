f9967f1fa6772225964048fdf017ceef694fec34
Shabuktagin Photon Khan <khan.photon@gmail.com>
2020-07-25 20:48:02 +0600

    test: fixed the posts test

--- src/__tests__/Services/Posts/Posts.test.tsx
+++ src/__tests__/Services/Posts/Posts.test.tsx
@@ -3,7 +3,6 @@ import { Posts } from '../../../Services/Posts'
 
 describe('Posts', () => {
     it('Calls axios and returns posts', async () => {
-        jest.mock('axios')
         // @ts-ignore
         axios.get.mockImplementationOnce((url, options) => Promise.resolve({
             data: [
@@ -20,6 +19,23 @@ describe('Posts', () => {
             config: {},
             headers: {},
         }))
+
         const response = await Posts({ params: { _limit: 20 } })
+
+        expect(response).toStrictEqual({
+            data: [
+                {
+                    department: "ENERSAVE",
+                    designation: "QUORDATE",
+                    id: 1,
+                    joining_date: "2014-07-10T04:50:36 -06:00",
+                    name: "Strong Cain",
+                }
+            ],
+            status: 200,
+            statusText: 'OK',
+            config: {},
+            headers: {},
+        })
     })
 })
