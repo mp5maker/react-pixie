2da724b75248faaedb6428c949821456037bc59e
Shabuktagin Photon Khan <khan.photon@gmail.com>
2020-07-26 22:40:51 +0600

    feat: add custom loader in the error page

--- src/Pages/Error/index.tsx
+++ src/Pages/Error/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { motion } from 'framer-motion'
 import get from 'lodash/get'
+import { useTranslation } from 'react-i18next'
 
 import { Colors } from 'Constants/Colors'
 import { PageTransition } from 'Constants/PageTransition'
@@ -11,11 +12,14 @@ import { ErrorMessage } from 'Components/ErrorMessage'
 import { Cards } from 'Components/Cards'
 import { useDocument } from 'Hooks/UseDocument'
 import { usePosts } from 'Hooks/UsePosts'
+import { SPK } from 'Svg/SPK'
+import "./styles.scss"
 
 export const Error = ({ history, location, match }: any) => {
     const { theme } = React.useContext(AppContext)
     const { setSettings, ...settings }: any = React.useContext(SettingsContext)
     const { loading, data } = usePosts({ params: { _limit: 20 } })
+    const { t } = useTranslation()
     useDocument({ options: [{ selector: 'title', value: `Photon's Portfolio: Page Do Not Exist` }] })
 
     React.useEffect(() => {
@@ -50,36 +54,98 @@ export const Error = ({ history, location, match }: any) => {
             </div>
             <div className="row">
                 <div className="col p-0">
-                    <Cards
-                        prepareItem={({ item }: any) => {
-                            const name = get(item, 'name', '')
-                            const designation = get(item, 'designation', '')
-                            const joining_date = get(item, 'joining_date', '')
-                            const department = get(item, 'department', '')
+                    {
+                        loading ? (
+                            <React.Fragment>
+                                <div className="loading-section">
+                                    <SPK
+                                        width={100}
+                                        height={46.8}
+                                        // @ts-ignore
+                                        fillColor={Colors[theme].primaryColor}
+                                        svgVariants={{}}
+                                        pathVariantsOne={{
+                                            initial: {
+                                                y: 0
+                                            },
+                                            animate: {
+                                                y: [0, 25, 50, 75, 50, 25],
+                                                transition: {
+                                                    y: {
+                                                        duration: 2,
+                                                        yoyo: Infinity
+                                                    },
+                                                }
+                                            },
+                                            exit: {}
+                                        }}
+                                        pathVariantsTwo={{
+                                            initial: {
+                                                y: 25
+                                            },
+                                            animate: {
+                                                y: [25, 50, 75, 50, 25, 0],
+                                                transition: {
+                                                    y: {
+                                                        duration: 2,
+                                                        yoyo: Infinity
+                                                    },
+                                                }
+                                            },
+                                            exit: {}
+                                        }}
+                                        pathVariantsThree={{
+                                            initial: {
+                                                y: 50
+                                            },
+                                            animate: {
+                                                y: [50, 75, 50, 25, 0, 25],
+                                                transition: {
+                                                    y: {
+                                                        duration: 2,
+                                                        yoyo: Infinity
+                                                    },
+                                                }
+                                            },
+                                            exit: {}
+                                        }}
+                                        colors={Colors}
+                                        theme={theme} />
+                                </div>
+                            </React.Fragment>
+                        ) : (
+                            <Cards
+                                prepareItem={({ item }: any) => {
+                                    const name = get(item, 'name', '')
+                                    const designation = get(item, 'designation', '')
+                                    const joining_date = get(item, 'joining_date', '')
+                                    const department = get(item, 'department', '')
 
-                            return (
-                                <React.Fragment>
-                                    <div>
-                                        <div>
-                                            <h5>
-                                                { name }
-                                            </h5>
-                                        </div>
-                                        <hr/>
-                                        <div className={`space-more`}>
-                                            { designation }
-                                        </div>
-                                        <div>
-                                            { department }
-                                        </div>
-                                        <div>
-                                            { joining_date.substring(0, 10) }
-                                        </div>
-                                    </div>
-                                </React.Fragment>
-                            )
-                        }}
-                        list={data} />
+                                    return (
+                                        <React.Fragment>
+                                            <div>
+                                                <div>
+                                                    <h5>
+                                                        { name }
+                                                    </h5>
+                                                </div>
+                                                <hr/>
+                                                <div className={`space-more`}>
+                                                    { designation }
+                                                </div>
+                                                <div>
+                                                    { department }
+                                                </div>
+                                                <div>
+                                                    { joining_date.substring(0, 10) }
+                                                </div>
+                                            </div>
+                                        </React.Fragment>
+                                    )
+                                }}
+                                list={data} />
+                        )
+                    }
                 </div>
             </div>
         </motion.div>
